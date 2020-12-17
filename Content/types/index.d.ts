6489ed4632ad896402a5c0b37a2dc85d67b07020
Emilis Makutėnas <emilis.makutenas@protonmail.com>
2020-12-17 22:35:45 +0200

    Clean project

--- Content/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import _React from 'react';
-import _ReactDOM from 'react-dom';
-import _ReactDOMServer from 'react-dom/server';
-
-declare global {
-  const React: typeof _React;
-  const ReactDOM: typeof _ReactDOM;
-  const ReactDOMServer: typeof _ReactDOMServer;
-}
