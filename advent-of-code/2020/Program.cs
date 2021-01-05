1189da6d5733ce560d12eda10299ab8a464f12f9
Emilis Makutėnas <emilis.makutenas@protonmail.com>
2021-01-06 00:48:03 +0200

    Add advent-of-code

---
+++ advent-of-code/2020/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _1A();
+            _1B();
+        }
+
+        public static void _1A()
+        {
+            var lines = Read(1).Select(line => int.Parse(line)).ToList();
+
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                for (int j = i; j < lines.Count; j++)
+                {
+                    if (lines[i] + lines[j] == 2020)
+                    {
+                        Console.WriteLine("{0}*{1}={2}", lines[i], lines[j], lines[i] * lines[j]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        public static void _1B()
+        {
+            var lines = Read(1).Select(line => int.Parse(line)).ToList();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                for (int j = i; j < lines.Count; j++)
+                {
+                    for (int k = j; k < lines.Count; k++)
+                    {
+                        if (lines[i] + lines[j] + lines[k] == 2020)
+                        {
+                            Console.WriteLine("{0}*{1}*{2}={3}", lines[i], lines[j], lines[k], lines[i] * lines[j] * lines[k]);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string[] Read(int level)
+        {
+            var path = Path.Join(Environment.CurrentDirectory, $"{level}\\input.txt");
+
+            return File.ReadAllLines(path);
+        }
+    }
+}
